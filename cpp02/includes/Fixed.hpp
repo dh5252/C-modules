e17dd1e769a679ae17ebc86d224848d75932edb2
chiwon <dh5277@naver.com>
2023-11-18 17:35:26 +0900

    correct error

---
+++ cpp02/includes/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chiwon <chiwon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 14:14:40 by chiwon            #+#    #+#             */
+/*   Updated: 2023/11/18 14:24:37 by chiwon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+    private :
+        int                 value;
+        const static int    bits = 8;
+    public :
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &obj);
+        Fixed& operator=(const Fixed &obj);
+        int getRawBits() const;
+        void setRawBits(int const raw);
+};
+
+
+#endif;
