2256d75e3a9952213d177716e72dd625b61e9abc
chiwon <dh5277@naver.com>
2024-04-02 23:29:02 +0900

    cpp06 ex00..ing

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,42 @@
+
+#include "includes/ScalarConverter.hpp"
+
+void ScalarConverter::convert(const std::string &input)
+{
+    double val;
+
+    char *ptr = NULL;
+    val = std::strtod(input.c_str(), &ptr);
+    try {
+        if (val == 0.0 &&
+        (input[0] != '+' && input[0] != '-' && !std::isdigit(input[0])) &&
+        (*ptr && std::strcmp(ptr, "f")))
+        throw std::exception();
+    } catch (std::exception &e) {
+        std::cout << "Convert fail" << std::endl;
+        return;
+    }
+
+    std::cout << "char: ";
+    if (std::isnan(val) || std::isinf(val)) 
+        std::cout << "impossible" << std::endl;
+    else if (std::isprint(static_cast<char>(val)))
+        std::cout << "'" << static_cast<char>(val) << "'" << std::endl;
+    else
+        std::cout << "Non displayable" << std::endl;
+
+    std::cout << "int: ";
+    if (std::isnan(val) || std::isinf(val) || val > INT_MAX || val < INT_MIN)
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << static_cast<int>(val) << std::endl;
+
+    std::cout << "float: ";
+    if (std::isnan(val) || std::isinf(val))
+        std::cout << std::showpos << static_cast<float>(val) << "f" << std::endl;
+    else if (static_cast<float>(val) == static_cast<int64_t>(static_cast<float>(val)))
+        std::cout << static_cast<float>(val) << ".0f" << std::endl;
+    else
+        std::cout << "float: " << std::setprecision(std::numeric_limits<float>::digits10) << static_cast<float>(val) << "f" << std::endl;
+
+}
---
+++ cpp06/ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+
+class ScalarConverter
+{
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter &obj);
+        ScalarConverter &operator=(const ScalarConverter &obj);
+
+    public:
+        ~ScalarConverter();
+        static void convert(const std::string &input);
+};
+
+#endif
